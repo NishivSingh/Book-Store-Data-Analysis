dfc76d66939df3e4d62b0001f755b56507386750
Nishiv Singh <nishiv2001@gmail.com>
2025-02-11 22:22:25 +0400

    Completed the basic queries

--- queries.sql
+++ queries.sql
@@ -62,5 +62,28 @@ WHERE Country = "Canada";
 SELECT * FROM Orders
 WHERE MONTH(Order_Date) = 11 AND YEAR(Order_Date) = 2023;
 
+-- 5) Retrieve the total stock of books available.
+SELECT SUM(Stock) AS total_stock
+FROM Books;
+
+-- 6) Find the details of the most expensive book.
+SELECT * FROM Books
+ORDER BY Price DESC
+LIMIT 1;
+
+-- 7) Show all customers who ordered more than 1 quantity of a book
+SELECT * FROM Orders
+WHERE quantity > 1;
+
+-- 8) Retrieve all orders where the total amount exceeds $20.
+SELECT * FROM Orders
+WHERE Total_Amount > 20;
+
+-- 9) List all genres available in the Books table.
+SELECT DISTINCT Genre FROM Books;
+
+-- 10) Calculate total revenue generated from all orders.
+SELECT SUM(Total_Amount) AS Total_Revenue FROM Orders;
+
 
 

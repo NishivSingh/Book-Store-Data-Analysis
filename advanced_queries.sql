7d4145e78f0d0aff364bf6c5bc032435a1e2b09e
Nishiv Singh <nishiv2001@gmail.com>
2025-02-18 22:17:09 +0400

    Working on the recent query

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -29,17 +29,3 @@ FROM AuthorRevenue
 WHERE Total_Revenue = (SELECT MAX(Total_Revenue) FROM AuthorRevenue);
 
 -- 3) Find the top 3 customers who spent most on their first order
-SELECT c.Customer_ID,c.Name,o.Order_Date, o.Total_Amount
-FROM Customers AS c
-JOIN Orders AS o
-ON c.Customer_ID = o.Customer_ID
-WHERE (c.Customer_ID,o.Total_Amount) IN
-(SELECT c.Customer_ID, MAX(o.Total_Amount)
-FROM Customers AS c
-JOIN Orders AS o
-ON c.Customer_ID = o.Customer_ID
-GROUP BY c.Customer_ID);  -- Incomplete query , currently finding the max amount along all orders
-
-SELECT * FROM Books;
-SELECT * FROM Customers;
-SELECT * FROM Orders;
